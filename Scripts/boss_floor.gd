c7a349248faf3f9674e8d3b43eb188165b54f89d
IDoTweaks <idotweaks@gmail.com>
2026-07-10 21:18:49 +0300

    adds working snake pathFinding

--- Scripts/boss_floor.gd
+++ Scripts/boss_floor.gd
@@ -1,10 +1,10 @@
 extends Node3D
 @onready var tile =$tileBody
-@onready var player = $Player2
-@onready var snake = $Snake
+@export var player : Node3D
+@export var snake : Node3D
 @export var width : int
 @export var length : int
-var floor
+var floor = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_genFloor(length,width,1.01)
@@ -76,3 +76,61 @@ func _randSelectTiles(ammount : int):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _wrld2Tile(x,z):
+	var best = Vector2i(-1,-1)
+	var bestDist = INF
+	for i in range(floor.size()):
+		for j in range(floor[i].size()):
+			var tile = floor[i][j];
+			if!is_instance_valid(tile):
+				continue
+			var distX = tile.global_position.x - x
+			var distZ = tile.global_position.z - z
+			var dist = (distX * distX) + (distZ * distZ)
+			if dist < bestDist:
+				bestDist = dist
+				best = Vector2i(i,j)
+	return best
+
+
+func _pathFind(fromX,fromZ,toX,toZ):
+	if floor == null:
+		return floor #complicated way to return null XD
+	var start = _wrld2Tile(fromX,fromZ)
+	var goal = _wrld2Tile(toX,toZ)
+	if start.x == -1 || goal.x == -1: return null
+	if start == goal:return floor[start.x][start.y]
+	var visited = {start: true}
+	var cameFrom = {}
+	var queue = [start]
+	var dirs = [Vector2i(1,0),Vector2i(-1,0),Vector2i(0,1),Vector2i(0,-1)]
+	var found = false
+	while queue.size() > 0:
+		var curr = queue.pop_front()
+		if curr == goal:
+			found = true
+			break
+		else:
+			pass
+		for d in dirs:
+			var nextX = curr.x + d.x
+			var nextZ = curr.y + d.y
+			if nextX < 0 or nextX >= floor.size():
+				continue
+			if nextZ < 0 or nextZ >= floor.size():
+				continue
+			var n = Vector2i(nextX,nextZ)
+			if visited.has(n):
+				continue
+			if !is_instance_valid(floor[nextX][nextZ]):
+				continue
+			visited[n] = true
+			cameFrom[n] = curr
+			queue.append(n)
+	if !found:
+		return null
+	var step = goal
+	while cameFrom.has(step) and cameFrom[step] != start:
+		step = cameFrom[step]
+	return floor[step.x][step.y]
---
+++ Scripts/snake.gd
@@ -0,0 +1,53 @@
+extends Node3D
+
+@export var bossFloor : Node3D
+@export var player : Node3D
+@export var tickTime : float = .4
+@export var stepTime : float = .15
+@export var jointDelay : float = .04
+var segms : Array = []
+var moving := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	segms = [$head,$hulia,$hulia2,$hulia3,$hulia4,$hulia5]
+	var timer = Timer.new()
+	timer.wait_time = tickTime
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(_onTick)
+
+func _onTick():
+	if !moving:
+		_step()
+
+func _step():
+	if bossFloor == null || player == null:
+		return
+	var head = segms[0]
+	var nextTile = bossFloor._pathFind(head.global_position.x,head.global_position.z, player.global_position.x,player.global_position.z)
+	if nextTile == null or !is_instance_valid(nextTile):
+		return
+	var targ = Vector3(nextTile.global_position.x,head.global_position.y,nextTile.global_position.z)
+	if head.global_position.distance_to(targ) < 0.01:
+		return
+	moving = true
+	var oldPos = []
+	for seg in segms:
+		oldPos.append(seg.global_position)
+	#var headDelta = targ -head.global_position
+	var dir = targ - head.global_position
+	var yaw = atan2(dir.z, -dir.x)#idfk know this math its the power of google!
+	yaw = head.rotation.y + wrapf(yaw - head.rotation.y,-PI,PI)
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(head,"global_position",targ,stepTime)
+	tween.tween_property(head,"rotation:y",yaw,stepTime)
+	for i in range(1,segms.size()):
+		tween.tween_property(segms[i],"global_position",oldPos[i-1],stepTime).set_delay(i * jointDelay)
+	await tween.finished
+	moving = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
