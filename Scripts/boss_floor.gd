3fdd3a070eea1237c298a43fb86d5cf54ad2b460
IDoTweaks <idotweaks@gmail.com>
2026-07-10 18:54:10 +0300

    adds working floor for the boss

--- Scripts/boss_floor.gd
+++ Scripts/boss_floor.gd
@@ -1,9 +1,35 @@
 extends Node3D
-@onready var tile =$tileBody/baseTile
-
+@onready var tile =$tileBody
+@onready var player = $Player2
+@onready var snake = $Snake
+@export var width : int
+@export var length : int
+var floor
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_genFloor(10,10,1.01)
+	_genFloor(length,width,1.01)
+	var selected = _randSelectTiles(100)
+	for i in selected:
+		i._activate(0)
+	
+
+func _selectAndShoot(toSelect : int):
+	var selected
+	if toSelect > width * length:
+		selected = _randSelectTiles(width * length)
+	else:
+		selected = _randSelectTiles(toSelect)
+	for i in selected:
+		i._activate(1)
+
+func _selectAndRemove(toSelect : int):
+	var selected
+	if toSelect > width * length:
+		selected = _randSelectTiles(width * length)
+	else:
+		selected = _randSelectTiles(toSelect)
+	for i in selected:
+		i._activate(0)
 
 func _genFloor(height : int, width : int,dist):
 	var arr = []
@@ -12,9 +38,11 @@ func _genFloor(height : int, width : int,dist):
 		for j in range(0,height):
 			var temp = tile.duplicate()
 			self.add_child(temp)
-			temp.create_convex_collision()
+			#temp.create_convex_collision()
 			arr[i].append(temp)
 	_arangeFloor(dist,arr,tile.position.x,tile.position.z)
+	floor = arr
+	tile.queue_free()
 
 func _arangeFloor(dist,arr : Array, baseX,baseZ):
 	var horiz = 0;
@@ -24,8 +52,26 @@ func _arangeFloor(dist,arr : Array, baseX,baseZ):
 		for j in range(0,arr[i].size()):
 			arr[i][j].position.x = baseX + (horiz * dist)
 			arr[i][j].position.z = baseZ + (vert * dist)
+			if i == arr.size() / 2 and j == arr[i].size() / 2:
+				player.global_position.x = arr[i][j].global_position.x
+				player.global_position.z = arr[i][j].global_position.z
+				snake.global_position.x = arr[i][j].global_position.x
+				snake.global_position.z = arr[i][j].global_position.z
 			vert+=1;
 		horiz+=1
+
+func _randSelectTiles(ammount : int):
+	var count : int = 0;
+	var selected : Array = []
+	while count < ammount:
+		for i in range(0,floor.size()):
+			for j in range(0,floor.size()):
+				if randi_range(1,250) == 5 and !selected.has(floor[i][j]):
+					selected.append(floor[i][j])
+					count+=1;
+				if count >= ammount:
+					return selected
+	return selected
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ Scripts/death_area.gd
@@ -0,0 +1,51 @@
+extends Area3D
+@onready var deathRay : MeshInstance3D = $deathRay
+var currDelt = 0.005
+var targ = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _activate(type : int):
+	await _warning(currDelt)
+	if type == 1:
+		_shoot()
+	else:
+		_removeSelf()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	currDelt = delta
+
+func _removeSelf():
+	get_parent().queue_free()
+
+func _shoot():
+	deathRay.visible = false
+	if targ != null:
+		targ._takeDamage(10000,self)
+	
+
+func _warning(delta: float):
+	deathRay.transparency = 1.0
+	deathRay.visible = true
+	var tween = create_tween()
+	tween.tween_property(deathRay,"transparency",0,2.5)
+	await tween.finished
+	#deathRay.transparency += .1 * delta
+	#print(deathRay.transparency)
+	#if deathRay.transparency >= 1:
+	#	deathRay.visible = false
+	#else:
+	#	_warning(currDelt)
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.has_method("_player"):
+		targ = body
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.has_method("_player"):
+		targ = null
